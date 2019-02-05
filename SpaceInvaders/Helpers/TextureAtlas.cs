2fbb028efc6790607e62002b0fc263b92bbe4800
GalacticGlum <nerftactic09@gmail.com>
2019-02-05 17:34:23 -0500

    Added texture atlas

---
+++ SpaceInvaders/Helpers/TextureAtlas.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Author: Shon Verch
+ * File Name: TextureAtlas.cs
+ * Project Name: SpaceInvaders
+ * Creation Date: 02/05/2019
+ * Modified Date: 02/05/2019
+ * Description: DESCRIPTION
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+
+namespace SpaceInvaders.Helpers
+{
+    public class TextureAtlasEntry
+    {
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name { get; set; }
+
+        [JsonProperty("rectangle", Required = Required.Always)]
+        public Rectangle Rectangle { get; set; }
+
+        [JsonIgnore]
+        public Texture2D Texture { get; set; } = null;
+    }
+
+    public class TextureAtlas
+    {
+        public Texture2D this[string name] => Get(name);
+
+        private readonly Texture2D textureAtlas;
+        private readonly Dictionary<string, TextureAtlasEntry> textureAtlasEntries;
+
+        private readonly GraphicsDevice graphicsDevice;
+
+        public TextureAtlas(string atlasContentFilePath, GraphicsDevice graphicsDevice, ContentManager contentManager)
+        {
+            this.graphicsDevice = graphicsDevice;
+
+            textureAtlas = contentManager.Load<Texture2D>(atlasContentFilePath);
+            textureAtlasEntries = new Dictionary<string, TextureAtlasEntry>();
+
+            string contentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content");
+            string atlasMetaDataFilePath = Path.ChangeExtension(Path.Combine(contentPath, atlasContentFilePath), "json");
+
+            LoadMetaData(atlasMetaDataFilePath);
+        }
+
+        public TextureAtlas(Texture2D textureAtlas, string atlasMetaDataFilePath, GraphicsDevice graphicsDevice)
+        {
+            this.graphicsDevice = graphicsDevice;
+            this.textureAtlas = textureAtlas;
+
+            textureAtlasEntries = new Dictionary<string, TextureAtlasEntry>();
+            LoadMetaData(atlasMetaDataFilePath);
+        }
+
+        private void LoadMetaData(string atlasMetaDataFilePath)
+        {
+            TextureAtlasEntry[] metaDataEntries = JsonConvert.DeserializeObject<TextureAtlasEntry[]>(atlasMetaDataFilePath);
+            foreach (TextureAtlasEntry entry in metaDataEntries)
+            {
+                textureAtlasEntries[entry.Name] = entry;
+            }
+        }
+
+        public Texture2D Get(string name, bool forceReload = false)
+        {
+            if (!textureAtlasEntries.ContainsKey(name)) return null;
+            TextureAtlasEntry entry = textureAtlasEntries[name];
+            if (!forceReload && entry.Texture != null)
+            {
+                return entry.Texture;
+            }
+
+            Texture2D texture = TextureHelpers.GetCroppedTexture(textureAtlas, entry.Rectangle, graphicsDevice);
+            entry.Texture = texture;
+
+            return texture;
+        }
+    }
+}
---
+++ SpaceInvaders/Helpers/TextureHelpers.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Author: Shon Verch
+ * File Name: TextureHelpers.cs
+ * Project Name: SpaceInvaders
+ * Creation Date: 02/05/2019
+ * Modified Date: 02/05/2019
+ * Description: DESCRIPTION
+ */
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceInvaders.Helpers
+{
+    public static class TextureHelpers
+    {
+        public static Texture2D GetCroppedTexture(Texture2D texture, Rectangle source, GraphicsDevice graphicsDevice)
+        {
+            Texture2D result = new Texture2D(graphicsDevice, source.Width, source.Height);
+            Color[] pixels = new Color[source.Width * source.Height];
+            texture.GetData(0, source, pixels, 0, pixels.Length);
+            result.SetData(pixels);
+
+            return result;
+        }
+    }
+}
